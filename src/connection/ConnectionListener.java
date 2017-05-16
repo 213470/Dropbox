b500a5c74ac54f6a257798f0e1dadd27402cf7ec
213470 <180218@edu.p.lodz.pl>
2017-05-16 19:32:32 +0200

    Client + Server

---
+++ src/connection/ConnectionListener.java
@@ -0,0 +1,54 @@
+package connection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ConnectionListener extends Thread {
+
+	private int port;
+
+	public ConnectionListener(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public void run() {
+		ServerSocket listener;
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("kk:mm:ss dd-MM-yyyy");
+		try {
+			listener = new ServerSocket(port);
+			System.out.println("Server stared!\nListeninig on port: " + port);
+			try {
+				while (true) {
+					Socket socket = listener.accept();
+					LocalDateTime userLoginTime = LocalDateTime.now();
+					
+					try {
+						BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						String username = input.readLine();
+						System.out.println("LOGGED: " + username);
+						
+						PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+						out.println(userLoginTime.format(dateFormat));
+					} finally {
+						socket.close();
+					}
+				}
+			} finally {
+				listener.close();
+			}
+		} catch (IOException e) {
+			System.out.println();
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
